c47b9d8070ff56ad777b386f0ae8e6afcc21eb31
Morgana <79321019+morganaschneider@users.noreply.github.com>
2023-09-12 02:20:22 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,4 +1,8 @@
 n=input('Digite algo:')
-print('È alfanumérico ?',n.isalnum())
+print('O tipo primitivo desse valor é', type(n))
+print('Só tem espaços ?',n.isspace())
+print ('É um numero ?',n.isalnum())
+print ('Está em maiscúlas?',n.isupper())
 print('Está em minúsculas ?',n.islower())
+print('Está capitalizada ?',n.istitle())
 
