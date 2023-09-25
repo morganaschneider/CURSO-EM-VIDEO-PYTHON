c907809b3ae6539979d97cea08abd8b3813d576d
Morgana <79321019+morganaschneider@users.noreply.github.com>
2023-09-25 08:14:09 -0300

    Update ex013.py

--- ex013.py
+++ ex013.py
@@ -1,3 +1,3 @@
-salário = float(input('Qual é o salário do Funcionário'))
-novo = salário + (salário * 15 / 100)
-print(f'Um funcionário que ganhava R${salário}, com 15% de aumento, passa a receber R${novo}')
+salario = float(input('Qual é o salário do Funcionário'))
+novo = salario + (salario * 15 / 100)
+print(f'Um funcionário que ganhava R${salario}, com 15% de aumento, passa a receber R${novo}')
