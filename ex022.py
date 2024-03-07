876bcf68651f32f1125d718a2c6bc989b9ac1c17
morganaschneider <morganaschneeider@gmail.com>
2024-03-06 22:33:41 -0300

    commit

---
+++ ex022.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome completo:'))
+print('Analisandoeu nome...')
+print(f'Seu nome em maíusculas é {nome.upper()}')
+print(f'Seu nome em minúsculas é {nome.lower()}')
---
+++ ex023.py
@@ -0,0 +1,2 @@
+num = int(input('Informe um número'))
+n =
