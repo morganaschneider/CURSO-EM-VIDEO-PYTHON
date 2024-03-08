968056bf514309ea8cf5bff2180b1be501e50f09
Morgana <79321019+morganaschneider@users.noreply.github.com>
2024-03-07 23:59:44 -0300

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -1,2 +1,10 @@
 num = int(input('Informe um número'))
-n =
+n = str(num)
+print(f'Analisando o número {num}')
+print(f'Unidade: {n[3]}')
+print(f'Centena: {n[2]}')
+print(f'Milhar: {n[0]}')
+
+
+
+
