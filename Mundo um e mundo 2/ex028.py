3bb2c872bcd41bf473ee1cd3c0d13f65dcb7d47e
morganaschneider <morganaschneeider@gmail.com>
2024-04-17 20:36:54 -0300

    r

--- Mundo um e mundo 2/ex028.py
+++ Mundo um e mundo 2/ex028.py
@@ -4,9 +4,3 @@ if nome == 'SIM':
 else:
     print('Nem queria mesmo!')
 
-nome = str(input('Quer namorar comigo?')).upper().strip()
-if nome == 'SIM':
-    print('To muito feliz, te amo!!')
-else:
-    print('Nem queria mesmo!')
-
