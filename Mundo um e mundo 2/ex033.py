26b3a4676c5ce176dd39874ad9b4d180300d06d5
morganaschneider <morganaschneeider@gmail.com>
2024-04-19 01:38:25 -0300

    r

---
+++ Mundo um e mundo 2/ex033.py
@@ -0,0 +1,9 @@
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+
+maior = max(a, b, c)
+menor = min(a, b, c)
+
+print(f'O maior valor digitado foi {maior}!')
+print(f'O menor valor digitado foi {menor}!')
