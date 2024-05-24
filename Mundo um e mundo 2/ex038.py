2b2b35a17c51454f9883b1c4f2cf050377c2ef55
morganaschneider <morganaschneeider@gmail.com>
2024-05-24 16:40:45 -0300

    r

---
+++ Mundo um e mundo 2/ex038.py
@@ -0,0 +1,8 @@
+n1 = int(input('Primeiro numero:'))
+n2 = int(input('Segundo numero:'))
+if n1 > n2:
+    print('O PRIMEIRO valor é maior')
+elif n1 > n2:
+    print('O SEGUNDO valor é menor')
+elif n1 == n2:
+    print('Os dois valores são iguais')
