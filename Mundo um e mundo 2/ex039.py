7a9e7d150eab3d1f3c697022dc1ce6460b952616
morganaschneider <morganaschneeider@gmail.com>
2024-05-24 17:12:31 -0300

    r

---
+++ Mundo um e mundo 2/ex039.py
@@ -0,0 +1,9 @@
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de Nascimento:'))
+idade = atual - nasc
+print(f'Quem nasceu em {nasc} tem {idade} em {atual}')
+if idade == 18:
+ print('Você tem que se alistar IMEDIATAMENTE')
+elif idade < 18:
+    print(f'Ainda faltam {18 - idade} anos para você se alistar, no ano de {nasc + 18}')
